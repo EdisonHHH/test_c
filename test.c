6eab9c1a5b1951412cc4811e1032a4c03ed1ea7f
Barry Song <21cnbao@gmail.com>
2018-11-21 20:04:09 +0800

    add test.c

---
+++ test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int a[12]={1,2,3,4,5,6,7,8,9,10,11,12};
+	int (*b)[4];
+	b=&a;
+	printf("%d\n",(*b)[0]);
+	b++;
+	printf("%d\n",(*b)[0]);
+
+	int *p[4];
+	p[0]=a;
+	printf("%d\n",*(++p[0]));
+	struct a{
+		int *b[4];
+	};
+	printf("%d\n",sizeof(struct a));
+	struct c{
+		int (*d)[4];
+	};
+	printf("%d\n",sizeof(struct c));
+	return 0;
+}
